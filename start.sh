5c8279fa6ba3b53b4f4028fb5e294b7bbd27ec52
aljaxus <me@aljaxus.eu>
2019-04-04 09:51:53 +0200

    Container now starts detached

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ printf "\n-> Removing simplewebchat container if exists\n"
 docker kill simplewebchat &>/dev/null
 docker rm simplewebchat &>/dev/null
 printf "\n-> Starting new container called zeyra\n"
-docker run -ti --rm --name simplewebchat -p 0.0.0.0:90:3000 webonize/simplewebchat
+docker run -d --rm --name simplewebchat -p 0.0.0.0:90:3000 webonize/simplewebchat
 echo ""
